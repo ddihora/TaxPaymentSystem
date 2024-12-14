67c5b2b8c4d2949970f26ee1b0c2011a6837759b
Dhvani Dihora <dhvanidihora@Dhvanis-MacBook-Air.local>
2024-12-14 15:08:26 -0500

    updated final files

--- setup.sql
+++ setup.sql
@@ -2,7 +2,7 @@ CREATE TABLE TaxPayments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     company TEXT NOT NULL,
     amount REAL,
-    payment_date DATE, -- No longer mandatory
+    payment_date DATE, 
     status TEXT CHECK(status IN ('paid', 'unpaid')) DEFAULT 'unpaid',
     due_date DATE NOT NULL -- Mandatory field
 );
